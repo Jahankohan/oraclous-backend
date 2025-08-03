c3b62368110f8cf2b26902120283a6c56e1534a8
Reza <reza@Rezas-MacBook-Pro-2.local>
2025-08-03 17:03:35 +0300

    Removed unused service

--- credential-broker-service/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.routes import credential_routes
-from app.core.lifespan import lifespan
-from app.core.auth_middleware import AuthMiddleware
-
-app = FastAPI(lifespan=lifespan)
-app.add_middleware(AuthMiddleware)
-
-app.include_router(credential_routes.router, prefix="/credentials", tags=["credentials"])
